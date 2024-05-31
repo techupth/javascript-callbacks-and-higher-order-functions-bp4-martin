10bc1a4fce3fc422342049eee5caf99fe245da84
Tinnaphop <tinnaphop.t@st.econ.tu.ac.th>
2024-05-31 21:22:41 +0700

    take assignment

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,16 @@
 // Exercise #1: For Each Function
 
 function forEach(array, operation) {
-  // Start coding here
-}
+  let result = []
+  for(let i = 0; i < array.length; i++) {
+    result.push(array[i] + operation)
+  }
+  return result
+};
 
-const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let employeeSalaries = [20005, 40000, 32000, 14500, 344000];
+let newEmployeeSalaries = [];
 
-// Using `forEach` function here
+newEmployeeSalaries = forEach(employeeSalaries, 5000);
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+console.log(newEmployeeSalaries);
